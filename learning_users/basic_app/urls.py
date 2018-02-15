6501c04c48293f380536969e921977394ec78bbc
practice git <user@email.com>
2018-02-15 10:26:28 +0000

    second

---
+++ learning_users/basic_app/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url,include
+from django.contrib import admin
+from basic_app import views
+
+#TEMPLATE URLS!
+
+app_name = "basic_app"
+
+urlpatterns = [
+    url(r"^register/$",views.register,name="register"),
+    url(r'^user_login/$',views.user_login,name="login"),
+    url(r'^logout/$',views.user_logout,name="logout"),
+    url(r'^special/',views.special,name="special"),
+]
